4addf421a27936564a656877886ab85a83367043
demirciouzhan <ouzd61@gmail.com>
2021-11-16 18:19:15 +0300

    post

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, NgModule, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+import { Post } from './post.module';
+import { PostsService } from './posts.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[]=[];
+  isFetching=false;
+
+  constructor(private http: HttpClient, private postsService: PostsService){
+
+  }
+
+  ngOnInit(){
+    this.postsService.fetchPosts();
+  }
+
+  onCreatePost(postData : Post){
+  console.log(postData);
+  this.postsService.createAndStorePost(postData.title, postData.content);
+  
+  }
+  onFetchPosts(){
+  this.postsService.fetchPosts();
+
+
+  }
+  onClearPosts(){
+
+  }
+ 
+}
